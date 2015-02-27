1be8da9c84d4bbfa0367a1238ce22c619d01a226
Nicola1971 <modxit@modx.it>
2015-02-27 18:43:23 +0100

    2.0
    Refactor for Dashboard 2.0 - boostrap based

---
+++ assets/plugins/welcomelistbox/listbox.php
@@ -0,0 +1,55 @@
+<?php
+/******
+WelcomeListBox 2.0 RC
+OnManagerWelcomePrerender,OnManagerWelcomeHome,OnManagerWelcomeRender
+
+&ListBoxEvoEvent= List Box placement:;list;OnManagerWelcomePrerender,OnManagerWelcomeHome,OnManagerWelcomeRender;OnManagerWelcomeHome &ListBoxSize= List Box size:;list;dashboard-block-full,dashboard-block-half;dashboard-block-half &ListMode= List Box mode:;list;basic,advanced;advanced &ListBoxTitle=Edit List documents Title:;string;List Box Widget &ParentFolder=Parent folder for List documents:;string;2 &ListItems=Max items in List:;string;20 &hideFolders= Hide Folders from List:;list;yes,no;no &dittolevel= Depht:;string;1
+****
+*/
+//blocks
+$ListOutput = isset($ListOutput) ? $ListOutput : '';
+//events
+$ListBoxEvoEvent = isset($ListBoxEvoEvent) ? $ListBoxEvoEvent : 'OnManagerWelcomeRender';
+// box size
+$ListBoxSize = isset($ListBoxSize) ? $ListBoxSize : 'dashboard-block-full';
+//widget grid size
+if ($ListBoxSize == 'dashboard-block-full') {
+$ListBoxWidth = 'col-sm-12';
+} else {
+$ListBoxWidth = 'col-sm-6';
+}
+$output = "";
+$e = &$modx->Event;
+
+/*List documents box*/
+if($e->name == ''.$ListBoxEvoEvent.'') {
+$parentId = $ParentFolder;
+$dittototal = $ListItems;
+	if ($ListMode == advanced) {
+$rowTpl = '@CODE: <li><a href="[(site_url)]index.php?id=[+id+]" target="_blank" title="preview"><i class="fa fa-eye green2"></i></a> <a href="index.php?a=27&id=[+id+]" title="edit"><i class="fa fa-pencil-square-o red2"></i></a> <a href="index.php?a=51&id=[+id+]" title="move"><i class="fa fa-arrows blueberry"></i></a> <a href="index.php?a=62&id=[+id+]" title="unpublish"><i class="fa fa-square-o blueberry"></i></a> <a href="index.php?a=6&id=[+id+]" title="delete"><i class="fa fa-trash-o red2"></i></a> <b>[+pagetitle+]</b> ([+id+])</li>';
+}
+	if ($ListMode == basic) {
+$rowTpl = '@CODE: <li><a href="[(site_url)]index.php?id=[+id+]" target="_blank" title="preview"><i class="fa fa-eye green2"></i></a> <a href="index.php?a=27&id=[+id+]" title="edit"><i class="fa fa-pencil-square-o red2"></i></a> <b>[+pagetitle+]</b> ([+id+])</li>';
+}
+$outerTpl = '@CODE: [+wf.wrapper+]';
+// Ditto parameters
+$params['parents'] = $parentId;
+$params['depth'] = $dittolevel;
+$params['tpl'] = $rowTpl;
+$params['total'] = $dittototal;
+if ($hideFolders == yes) {
+$params['hideFolders'] = '1';
+}
+	if ($hideFolders == no) {
+$params['hideFolders'] = '0';
+}
+// run Ditto
+$list = $modx->runSnippet('Ditto', $params);
+$ListOutput = '<div class="'.$ListBoxWidth.'"><div class="widget-wrapper"><div class="widget-title sectionHeader"><i class="fa fa-pencil"></i> '.$ListBoxTitle.'</div>
+<div class="widget-stage sectionBody"><ul>'.$list.'</ul><br style="clear:both;height:1px;margin-top: -1px;line-height:1px;font-size:1px;" /> </div></div></div>';
+}
+//end list
+$output = $ListOutput;
+$e->output($output);
+return;
+?>
