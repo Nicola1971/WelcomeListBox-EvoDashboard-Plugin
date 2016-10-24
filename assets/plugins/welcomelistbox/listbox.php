3305a711fcb7908c3f493b9311b2ce77e674a977
Nicola Lambathakis <modxit@modx.it>
2016-10-24 16:16:31 +0200

    removed basic mode config
    and fixed icons

--- assets/plugins/welcomelistbox/listbox.php
+++ assets/plugins/welcomelistbox/listbox.php
@@ -2,7 +2,7 @@
 /******
 WelcomeDocumentsListBox 3.1 RC
  * 
-&WidgetTitle= Widget title:;string;List Documents &WidgetIcon= Widget Icon:;string;fa-pencil &ListMode= List Box mode:;list;basic,advanced;advanced &ParentFolder=Parent folder for List documents:;string;2,15 &ListItems=Max items in List:;string;20 &tablefields= Tv Fields:;string;[+longtitle+],[+description+],[+introtext+],[+documentTags+] &tableheading=TV  heading:;string;Long Title,Description,Introtext,Tags &hideFolders= Hide Folders:;list;yes,no;yes &showPublishedOnly= Show Published Only:;list;yes,no;no &dittolevel= Depht:;string;1 &showMoveButton= Show Move Button:;list;yes,no;yes &showPublishButton= Show Publish Button:;list;yes,no;yes &showDeleteButton= Show Delete Button:;list;yes,no;yes &datarow= widget row position:;list;1,2,3,4,5,6,7,8,9,10;2 &datacol= widget col position:;list;1,2,3,4;1 &datasizex= widget x size:;list;1,2,3,4;2 &datasizey= widget y size:;list;1,2,3,4,5,6,7,8,9,10;4 &WidgetID= Unique Widget ID:;string;DocListBox 
+&WidgetTitle= Widget title:;string;List Documents &WidgetIcon= Widget Icon:;string;fa-pencil &ParentFolder=Parent folder for List documents:;string;2,15 &ListItems=Max items in List:;string;20 &tablefields= Tv Fields:;string;[+longtitle+],[+description+],[+introtext+],[+documentTags+] &tableheading=TV  heading:;string;Long Title,Description,Introtext,Tags &hideFolders= Hide Folders:;list;yes,no;yes &showPublishedOnly= Show Published Only:;list;yes,no;no &dittolevel= Depht:;string;1 &showMoveButton= Show Move Button:;list;yes,no;yes &showPublishButton= Show Publish Button:;list;yes,no;yes &showDeleteButton= Show Delete Button:;list;yes,no;yes &datarow= widget row position:;list;1,2,3,4,5,6,7,8,9,10;2 &datacol= widget col position:;list;1,2,3,4;1 &datasizex= widget x size:;list;1,2,3,4;2 &datasizey= widget y size:;list;1,2,3,4,5,6,7,8,9,10;4 &WidgetID= Unique Widget ID:;string;DocListBox 
 
 
 ****
@@ -42,28 +42,27 @@ $e = &$modx->Event;
 if($e->name == ''.$EvoEvent.'') {
 $parentId = $ParentFolder;
 $dittototal = $ListItems;
-	if ($ListMode == advanced) {
 $rowTpl = '@CODE: <tr>
 <td data-toggle="collapse" data-target=".collapse'.$WidgetID.'[+id+]"> <span class="label label-info">[+id+]</span></td>
 <td><a class="[[if? &is=`[+published+]:=:0` &then=`unpublished`]]" href="index.php?a=27&id=[+id+]" title="edit">[+pagetitle+]</a></td>
 <td>[+date+]</td>
 <td style="text-align: right;">
-<a class="btn btn-xs btn-success btn-action" href="index.php?a=27&id=[+id+]" title="edit"><i class="fa fa-pencil-square-o icon-color-red icon-no-border"></i></a> 
-<a class="btn btn-xs btn-info btn-action" href="[(site_url)]index.php?id=[+id+]" target="_blank" title="preview"><i class="fa fa-eye icon-color-light-green icon-no-border"></i></a>
+<a class="btn btn-xs btn-success btn-action" href="index.php?a=27&id=[+id+]" title="edit"><i class="fa fa-pencil-square-o"></i></a> 
+<a class="btn btn-xs btn-info btn-action" href="[(site_url)]index.php?id=[+id+]" target="_blank" title="preview"><i class="fa fa-eye"></i></a>
 ';
 if ($showMoveButton == yes) { 
-$rowTpl .= '<a class="btn btn-xs btn-move btn-action" href="index.php?a=51&id=[+id+]" title="move"><i class="fa fa-arrows icon-color-blue icon-no-border"></i></a>';
+$rowTpl .= '<a class="btn btn-xs btn-move btn-action" href="index.php?a=51&id=[+id+]" title="move"><i class="fa fa-arrows"></i></a>';
 }
 if ($showPublishButton == yes) { 
 $rowTpl .= '[[if? &is=`[+published+]:=:1` &then=` 
-<a class="btn btn-xs btn-warning btn-action" href="index.php?a=62&id=[+id+]" title="unpublish"><i class="fa fa-arrow-down icon-color-grey icon-no-border"></i></a> 
+<a class="btn btn-xs btn-warning btn-action" href="index.php?a=62&id=[+id+]" title="unpublish"><i class="fa fa-arrow-down"></i></a> 
 `&else=`
-<a class="btn btn-xs btn-primary btn-action" href="index.php?a=62&id=[+id+]" title="publish"><i class="fa fa-arrow-up icon-color-grey icon-no-border"></i></a> 
+<a class="btn btn-xs btn-primary btn-action" href="index.php?a=62&id=[+id+]" title="publish"><i class="fa fa-arrow-up"></i></a> 
 `]]';
 }
  
 if ($showDeleteButton == yes) { 
-$rowTpl .= '<a class="btn btn-xs btn-danger btn-action" href="index.php?a=6&id=[+id+]" title="delete"><i class="fa fa-trash-o icon-color-red icon-no-border"></i></a> ';
+$rowTpl .= '<a class="btn btn-xs btn-danger btn-action" href="index.php?a=6&id=[+id+]" title="delete"><i class="fa fa-trash-o"></i></a> ';
 }
 
 $rowTpl .= '<button class="btn btn-xs btn-default btn-expand btn-action" title="' . $_lang["resource_overview"] . '" data-toggle="collapse" data-target=".collapse'.$WidgetID.'[+id+]"><i class="fa fa-info" aria-hidden="true"></i></button></td>
@@ -76,14 +75,7 @@ $rowTpl .= '<button class="btn btn-xs btn-default btn-expand btn-action" title="
 </td>
 </tr>
 ';
-}
-	if ($ListMode == basic) {
-$rowTpl = '@CODE: <tr>
-<td width="5%"><a href="[(site_url)]index.php?id=[+id+]" target="_blank" title="preview"><i class="fa fa-eye green2"></i></a></td>
-<td width="5%"><a href="index.php?a=27&id=[+id+]" title="edit"><i class="fa fa-pencil-square-o icon-color-red icon-no-border"></i></a> </td>
-<td><b>[+pagetitle+]</b> ([+id+])</td>
-</tr>';
-}
+
 // Ditto parameters
 $params['parents'] = $parentId;
 $params['depth'] = $dittolevel;
@@ -119,7 +111,7 @@ $WidgetOutput = '
                           <span class="pull-left"><i class="fa '.$WidgetIcon.'"></i> '.$WidgetTitle.'</span>
                             <div class="widget-controls pull-right">
                                 <div class="btn-group">
-                                    <a href="#" class="btn btn-default btn-xs panel-hide hide-full glyphicon glyphicon-minus" data-id="'.$WidgetID.'"></a>
+                                    <a href="#" class="btn btn-default btn-xs panel-hide hide-full fa fa-minus" data-id="'.$WidgetID.'"></a>
                                 </div>     
                             </div>
                       </div>
